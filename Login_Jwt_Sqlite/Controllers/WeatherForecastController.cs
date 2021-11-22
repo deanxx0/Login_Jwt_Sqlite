984cf005d359506aad66863380d21b0b2b88ab5b
deanxx <deanxx999@gmail.com>
2021-11-22 19:12:04 +0900

    add authorize on weather controller for test

--- Login_Jwt_Sqlite/Controllers/WeatherForecastController.cs
+++ Login_Jwt_Sqlite/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -9,6 +10,7 @@ namespace Login_Jwt_Sqlite.Controllers
 {
     [ApiController]
     [Route("[controller]")]
+    [Authorize(Policy = "AdminOnly")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
